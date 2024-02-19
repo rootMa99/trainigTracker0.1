0f0583d07b7ba604f3373facfa5e16f22ea167c8
anass zeroual <anasszeroual09@gmail.com>
2024-02-19 12:15:25 +0100

    cp

---
+++ src/main/java/com/aptiv/trainig_tracker/TrainigTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aptiv.trainig_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainigTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainigTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aptiv/trainig_tracker/TrainigTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptiv.trainig_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainigTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
