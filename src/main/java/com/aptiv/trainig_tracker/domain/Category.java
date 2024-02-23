89f519a0b4e4ea0580b6efca2697cbc5eee7d4d2
anass zeroual <anasszeroual09@gmail.com>
2024-02-23 10:32:14 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/domain/Category.java
+++ src/main/java/com/aptiv/trainig_tracker/domain/Category.java
@@ -1,5 +1,14 @@
 package com.aptiv.trainig_tracker.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Category {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "categoryName")
+    private String categoryName;
 }
