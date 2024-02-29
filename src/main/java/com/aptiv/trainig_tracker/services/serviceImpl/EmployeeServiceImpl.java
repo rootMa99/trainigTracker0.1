823a42882d45efdad7ce261063524f097b2d2d3c
anass zeroual <anasszeroual09@gmail.com>
2024-02-29 15:28:56 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/services/serviceImpl/EmployeeServiceImpl.java
+++ src/main/java/com/aptiv/trainig_tracker/services/serviceImpl/EmployeeServiceImpl.java
@@ -6,8 +6,6 @@ import com.aptiv.trainig_tracker.repositories.*;
 import com.aptiv.trainig_tracker.services.EmployeeService;
 import com.aptiv.trainig_tracker.services.UploadEmployeeData;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +17,7 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
     CategoryRepo categoryRepo;
     CoordinatorRepo coordinatorRepo;
     CrewRepo crewRepo;
@@ -31,9 +30,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeModel getEmployeeData(long matricule) {
-        Employee employee= employeeRepo.findByMatricule(matricule);
-        EmployeeModel employeeModel=new EmployeeModel();
-        if (employee!=null){
+        Employee employee = employeeRepo.findByMatricule(matricule);
+        EmployeeModel employeeModel = new EmployeeModel();
+        if (employee != null) {
             employeeModel.setMatricule(employee.getMatricule());
             employeeModel.setNom(employee.getNom());
             employeeModel.setPrenom(employee.getPrenom());
@@ -46,8 +45,8 @@ public class EmployeeServiceImpl implements EmployeeService {
             employeeModel.setCoordinator(employee.getCoordinator().getName());
             employeeModel.setShiftLeader(employee.getShiftLeader().getName());
             employeeModel.setTeamLeader(employee.getTeamLeader().getName());
-            List<TrainingFromExcel>trainingFromExcels= new ArrayList<>();
-            for (Training t: employee.getTrainings()){
+            List<TrainingFromExcel> trainingFromExcels = new ArrayList<>();
+            for (Training t : employee.getTrainings()) {
                 TrainingFromExcel trainingFromExcel = getTrainingFromExcel(t);
                 trainingFromExcels.add(trainingFromExcel);
             }
@@ -57,98 +56,96 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     private static TrainingFromExcel getTrainingFromExcel(Training t) {
-        TrainingFromExcel trainingFromExcel = getFromExcel(t);
-        return trainingFromExcel;
+        return getFromExcel(t);
     }
 
 
-
     @Override
-    public void saveEmployeeDataToDb(MultipartFile file) throws IllegalAccessException {
-        ModelMapper mp=new ModelMapper();
-        mp.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        if (UploadEmployeeData.isValidFormat(file)){
+    public void saveEmployeeDataToDb(MultipartFile file) {
+
+        if (UploadEmployeeData.isValidFormat(file)) {
             try {
-                List<DataExcelEmployee>dataExcelEmployees= UploadEmployeeData.getEmployeesDataFromExcel(file.getInputStream());
-                List<Employee>employees=new ArrayList<>();
-                for (DataExcelEmployee dee : dataExcelEmployees){
-                    Employee employee=new Employee();
+                List<DataExcelEmployee> dataExcelEmployees = UploadEmployeeData.getEmployeesDataFromExcel(file.getInputStream());
+                List<Employee> employees = new ArrayList<>();
+                for (DataExcelEmployee dee : dataExcelEmployees) {
+                    Employee employee = new Employee();
                     employee.setMatricule(dee.getMatricule());
                     employee.setNom(dee.getNom());
                     employee.setPrenom(dee.getPrenom());
                     employee.setFonctionEntreprise(dee.getFonction());
-                    Category category= categoryRepo.findByCategoryName(dee.getCategory());
-                    if (category==null){
-                        Category ct= new Category();
+                    Category category = categoryRepo.findByCategoryName(dee.getCategory());
+                    if (category == null) {
+                        Category ct = new Category();
                         ct.setCategoryName(dee.getCategory());
-                        category= categoryRepo.save(ct);
+                        category = categoryRepo.save(ct);
                     }
                     employee.setCategory(category);
-                    Crew crew= crewRepo.findByCrewName(dee.getCrew());
-                    if (crew==null){
-                        Crew cr= new Crew();
+                    Crew crew = crewRepo.findByCrewName(dee.getCrew());
+                    if (crew == null) {
+                        Crew cr = new Crew();
                         cr.setCrewName(dee.getCrew());
-                        crew= crewRepo.save(cr);
+                        crew = crewRepo.save(cr);
                     }
                     employee.setCrew(crew);
-                    Poste poste= posteRepo.findByPosteName(dee.getPoste());
-                    if (poste==null){
-                        Poste ps=new Poste();
+                    Poste poste = posteRepo.findByPosteName(dee.getPoste());
+                    if (poste == null) {
+                        Poste ps = new Poste();
                         ps.setPosteName(dee.getPoste());
-                        poste= posteRepo.save(ps);
+                        poste = posteRepo.save(ps);
                     }
                     employee.setPoste(poste);
-                    Family family=familyRepo.findByFamilyName(dee.getFamily());
-                    if (family==null){
-                        Family fm=new Family();
+                    Family family = familyRepo.findByFamilyName(dee.getFamily());
+                    if (family == null) {
+                        Family fm = new Family();
                         fm.setFamilyName(dee.getFamily());
-                        family=familyRepo.save(fm);
+                        family = familyRepo.save(fm);
                     }
                     employee.setFamily(family);
-                    Department department= departmentRepo.findByDepartmentName(dee.getDepartment());
-                    if (department==null){
-                        Department dprt=new Department();
+                    Department department = departmentRepo.findByDepartmentName(dee.getDepartment());
+                    if (department == null) {
+                        Department dprt = new Department();
                         dprt.setDepartmentName(dee.getDepartment());
-                        department=departmentRepo.save(dprt);
+                        department = departmentRepo.save(dprt);
                     }
                     employee.setDepartment(department);
-                    Coordinator coordinator= coordinatorRepo.findByName(dee.getCoordinator());
-                    if (coordinator==null){
-                        Coordinator c=new Coordinator();
+                    Coordinator coordinator = coordinatorRepo.findByName(dee.getCoordinator());
+                    if (coordinator == null) {
+                        Coordinator c = new Coordinator();
                         c.setName(dee.getCoordinator());
-                        coordinator= coordinatorRepo.save(c);
+                        coordinator = coordinatorRepo.save(c);
                     }
                     employee.setCoordinator(coordinator);
-                    ShiftLeader shiftLeader= shiftLeaderRepo.findByName(dee.getShiftLeader());
-                    if (shiftLeader==null){
-                        ShiftLeader sl= new ShiftLeader();
+                    ShiftLeader shiftLeader = shiftLeaderRepo.findByName(dee.getShiftLeader());
+                    if (shiftLeader == null) {
+                        ShiftLeader sl = new ShiftLeader();
                         sl.setName(dee.getShiftLeader());
-                        shiftLeader= shiftLeaderRepo.save(sl);
+                        shiftLeader = shiftLeaderRepo.save(sl);
                     }
                     employee.setShiftLeader(shiftLeader);
-                    TeamLeader teamLeader= teamLeaderRepo.findByName(dee.getTeamLeader());
-                    if (teamLeader==null){
-                        TeamLeader tl= new TeamLeader();
+                    TeamLeader teamLeader = teamLeaderRepo.findByName(dee.getTeamLeader());
+                    if (teamLeader == null) {
+                        TeamLeader tl = new TeamLeader();
                         tl.setName(dee.getTeamLeader());
-                        teamLeader= teamLeaderRepo.save(tl);
+                        teamLeader = teamLeaderRepo.save(tl);
                     }
                     employee.setTeamLeader(teamLeader);
                     employees.add(employee);
                 }
                 employeeRepo.saveAll(employees);
-            }catch (IOException e){
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
+
     @Override
-    public CrewDto getCrewName(String crewName){
-        Crew crew= crewRepo.findByCrewName(crewName);
-        CrewDto crewDto=new CrewDto();
+    public CrewDto getCrewName(String crewName) {
+        Crew crew = crewRepo.findByCrewName(crewName);
+        CrewDto crewDto = new CrewDto();
         crewDto.setCrewName(crew.getCrewName());
-        List<EmployeeModel> employeeRests=new ArrayList<>();
-        for (Employee e: crew.getEmployees()){
-            EmployeeModel em=new EmployeeModel();
+        List<EmployeeModel> employeeRests = new ArrayList<>();
+        for (Employee e : crew.getEmployees()) {
+            EmployeeModel em = new EmployeeModel();
             em.setMatricule(e.getMatricule());
             em.setNom(e.getNom());
             em.setPrenom(e.getPrenom());
@@ -161,8 +158,8 @@ public class EmployeeServiceImpl implements EmployeeService {
             em.setCoordinator(e.getCoordinator().getName());
             em.setShiftLeader(e.getShiftLeader().getName());
             em.setTeamLeader(e.getTeamLeader().getName());
-            List<TrainingFromExcel> trainingFromExcels=new ArrayList<>();
-            for (Training t:e.getTrainings()){
+            List<TrainingFromExcel> trainingFromExcels = new ArrayList<>();
+            for (Training t : e.getTrainings()) {
                 TrainingFromExcel trainingFromExcel = getFromExcel(t);
                 trainingFromExcels.add(trainingFromExcel);
             }
@@ -174,7 +171,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     private static TrainingFromExcel getFromExcel(Training t) {
-        TrainingFromExcel trainingFromExcel=new TrainingFromExcel();
+        TrainingFromExcel trainingFromExcel = new TrainingFromExcel();
         trainingFromExcel.setTrainingId(t.getTrainingId());
         trainingFromExcel.setTrainingTitle(t.getTrainingTitle().getTrainingTitleName());
         trainingFromExcel.setTrainingType(t.getTrainingType().getTtName());
