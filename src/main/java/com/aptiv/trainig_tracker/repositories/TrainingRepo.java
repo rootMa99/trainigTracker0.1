3ee975b4365bdd75a10ff50e98f4ade44e913621
anass zeroual <anasszeroual09@gmail.com>
2024-03-04 14:30:05 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/repositories/TrainingRepo.java
+++ src/main/java/com/aptiv/trainig_tracker/repositories/TrainingRepo.java
@@ -10,10 +10,13 @@ import java.util.List;
 @Repository
 public interface TrainingRepo extends JpaRepository<Training, Long> {
     List<Training> findAllByDateDebutBetween(Date startDate, Date endDate);
+
     Training findByTrainingId(String trainingId);
+
     Training findByTrainingTypeTtNameAndTrainingTitleTrainingTitleNameAndDateDebutBetween(String trainingType,
                                                                                           String trainingTitle,
                                                                                           Date startDate, Date endDate);
+
     List<Training> findAllByTrainingId(String trainingId);
 
     List<Training> findAllByTrainingTypeTtNameAndTrainingTitleTrainingTitleNameAndDateDebutBetween(String trainingType, String trainingTitle, Date ddb, Date ddf);
