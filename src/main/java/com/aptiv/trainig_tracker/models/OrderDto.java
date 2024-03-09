83cbee44a70f76c525d40e141bfd5489363fc1d4
anass zeroual <anasszeroual09@gmail.com>
2024-03-09 11:26:11 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/models/OrderDto.java
+++ src/main/java/com/aptiv/trainig_tracker/models/OrderDto.java
@@ -1,6 +1,21 @@
 package com.aptiv.trainig_tracker.models;
 
-public class OrderDto {
-    private String Qualification;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class OrderDto {
+    private String qualification;
+    private Date orderdate;
+    private String shiftLeaderName;
+    private String shift;
+    private List<Long> matricules;
 }
