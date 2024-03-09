db785f159c0e42e92fe6b819fa85ddcc375358ba
anass zeroual <anasszeroual09@gmail.com>
2024-03-09 11:59:56 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/services/serviceImpl/OtherServiceImpl.java
+++ src/main/java/com/aptiv/trainig_tracker/services/serviceImpl/OtherServiceImpl.java
@@ -1,9 +1,13 @@
 package com.aptiv.trainig_tracker.services.serviceImpl;
 
+import com.aptiv.trainig_tracker.models.OrderDto;
+import com.aptiv.trainig_tracker.models.StatusRest;
 import com.aptiv.trainig_tracker.repositories.*;
 import com.aptiv.trainig_tracker.services.OtherService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -21,4 +25,7 @@ public class OtherServiceImpl implements OtherService {
     ShiftLeaderRepo shiftLeaderRepo;
     TrainingTitleRepo trainingTitleRepo;
 
+    public StatusRest saveOrderToDb(List<OrderDto> orderDtoList){
+        return null;
+    }
 }
