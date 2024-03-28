db6e6a0e6217f22292c77a46a3e5391ffddffaf1
rootMa99 <anasszeroual09@gmail.com>
2024-03-28 20:58:30 +0000

    cp

--- src/main/java/com/aptiv/trainig_tracker/config/SecurityConfiguration.java
+++ src/main/java/com/aptiv/trainig_tracker/config/SecurityConfiguration.java
@@ -25,6 +25,7 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserService userService;
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
