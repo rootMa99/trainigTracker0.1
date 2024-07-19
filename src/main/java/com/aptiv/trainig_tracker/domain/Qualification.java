1e7e047ed7276c74e20910ed3687f3a7bd20a7e6
anass zeroual <anasszeroual09@gmail.com>
2024-07-19 09:40:56 +0100

    cp

---
+++ src/main/java/com/aptiv/trainig_tracker/domain/Qualification.java
@@ -0,0 +1,20 @@
+package com.aptiv.trainig_tracker.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity(name = "qualification")
+@Data
+public class Qualification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name = "Qualification_Employee_Mapping",joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "matricule")
+    )
+    private List<Employee> employees;
+}
