07ba4ebb7afa4a7ce15fd8b79c6d19c784ca813f
anass zeroual <anasszeroual09@gmail.com>
2024-07-19 15:14:24 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/domain/QualificationEmployee.java
+++ src/main/java/com/aptiv/trainig_tracker/domain/QualificationEmployee.java
@@ -1,4 +1,12 @@
 package com.aptiv.trainig_tracker.domain;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity(name = "qualification_employee")
+@Data
 public class QualificationEmployee {
+    @EmbeddedId
+    private QualificationEmployeeId id;
 }
