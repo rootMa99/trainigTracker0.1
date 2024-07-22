9b1b99a22aaf0b1645ceb936c3a0426138122cc4
anass zeroual <anasszeroual09@gmail.com>
2024-07-22 11:29:50 +0100

    cp

--- src/main/java/com/aptiv/trainig_tracker/repositories/QualificationEmployeeRepo.java
+++ src/main/java/com/aptiv/trainig_tracker/repositories/QualificationEmployeeRepo.java
@@ -3,6 +3,8 @@ package com.aptiv.trainig_tracker.repositories;
 import com.aptiv.trainig_tracker.domain.QualificationEmployee;
 import com.aptiv.trainig_tracker.domain.QualificationEmployeeId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QualificationEmployeeRepo extends JpaRepository<QualificationEmployee, QualificationEmployeeId> {
 }
--- src/main/java/com/aptiv/trainig_tracker/repositories/QualificationRepo.java
+++ src/main/java/com/aptiv/trainig_tracker/repositories/QualificationRepo.java
@@ -2,6 +2,9 @@ package com.aptiv.trainig_tracker.repositories;
 
 import com.aptiv.trainig_tracker.domain.Qualification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QualificationRepo extends JpaRepository<Qualification, Long> {
+    Qualification findByName(String name);
 }
